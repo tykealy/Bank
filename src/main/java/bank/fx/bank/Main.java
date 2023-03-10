43c741a375910aae12f3ac0768b8a5ffbf1a7a80
Tykea <Tykeaboyloy@gmail.com>
2023-03-10 14:58:05 +0700

    refined

--- src/main/java/bank/fx/bank/Main.java
+++ src/main/java/bank/fx/bank/Main.java
@@ -11,15 +11,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainScene.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Name Bank");
         stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/dog.jpg")));
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) throws SQLException {
         launch();
     }
 }
-
