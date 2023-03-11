4dc8d377548f903aa030be94eab668f32bad577f
Prak Pichey <picheyprak@gmail.com>
2023-03-12 00:06:48 +0700

    create account functionn and main account scene

--- src/main/java/bank/fx/bank/User.java
+++ src/main/java/bank/fx/bank/User.java
@@ -1,7 +1,6 @@
 package bank.fx.bank;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class User {
@@ -17,13 +16,13 @@ public class User {
         String salt = Encryption.getSaltvalue(30);
         try {
             PreparedStatement stmt = Database.create(sqlString);
-            stmt.setString(1, this.firstName);
-            stmt.setString(2, this.lastName);
-            stmt.setInt(3, this.age);
-            stmt.setString(4, this.nationality);
-            stmt.setString(5, this.idCard);
-            stmt.setString(6, this.phone);
-            stmt.setString(7, this.email);
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+            stmt.setInt(3, age);
+            stmt.setString(4, nationality);
+            stmt.setString(5, idCard);
+            stmt.setString(6, phone);
+            stmt.setString(7, email);
             stmt.setString(8, Encryption.encrypt(this.password, salt));
             stmt.setString(9, salt);
             stmt.executeUpdate();
