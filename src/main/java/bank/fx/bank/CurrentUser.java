994ddde76934ddcfec8fcf2a42d89bfbe23bc0d3
Tykea <Tykeaboyloy@gmail.com>
2023-03-12 17:06:04 +0700

    can now get all the accounts in the current user and put it inside CurrentUser class

--- src/main/java/bank/fx/bank/CurrentUser.java
+++ src/main/java/bank/fx/bank/CurrentUser.java
@@ -2,10 +2,12 @@ package bank.fx.bank;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CurrentUser extends User {
     public static String firstName, lastName, nationality, idCard, phone, email, password, password_salt;
     public static int id, age;
+    public static ArrayList<Account> accounts = new ArrayList<Account>();
 
     public static void setCurrentUser(ResultSet rs) throws SQLException {
         firstName = rs.getString("first_name");
@@ -14,5 +16,9 @@ public class CurrentUser extends User {
         email = rs.getString("email");
         id = rs.getInt("id");
         age = rs.getInt("age");
+    }
+
+    public static void getAccounts() throws SQLException {
+        accounts = Account.get();
     }
 }
