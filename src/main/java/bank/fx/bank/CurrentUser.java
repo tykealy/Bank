69a17d08d2d6c376780f8e93512e2e9195842038
Tykea <Tykeaboyloy@gmail.com>
2023-03-12 14:28:43 +0700

    fixed master login error

--- src/main/java/bank/fx/bank/CurrentUser.java
+++ src/main/java/bank/fx/bank/CurrentUser.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CurrentUser extends User {
+    public static String firstName, lastName, nationality, idCard, phone, email, password, password_salt;
+    public static int id, age;
 
     public static void setCurrentUser(ResultSet rs) throws SQLException {
         firstName = rs.getString("first_name");
