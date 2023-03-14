9a1fc194374f553ee9ba109905dca6355d16c0cb
Tykea <Tykeaboyloy@gmail.com>
2023-03-14 15:12:03 +0700

    refined some ui

--- src/main/java/bank/fx/bank/Main.java
+++ src/main/java/bank/fx/bank/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CardScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("The banK");
         stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/appLogo.png")));
