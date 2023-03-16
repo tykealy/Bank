9a61b9d0aa67b1777f55de9c1d5bb059bcdf7538
QILIN <sengheng.measheanh@gmail.com>
2023-03-16 16:20:40 +0700

    add transfer UI and test

--- src/main/java/bank/fx/bank/Controller/confirmTransferController.java
+++ src/main/java/bank/fx/bank/Controller/confirmTransferController.java
@@ -1,4 +1,28 @@
 package bank.fx.bank.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class confirmTransferController {
+    @FXML
+    Label amountConfirm,accountNoConfirm,receiverConfirm;
+
+    public void popupConfirmTransfer(Parent root) throws IOException {
+        Stage popupWindow = new Stage();
+        Scene scene = new Scene(root);
+        popupWindow.setScene(scene);
+        popupWindow.initModality(Modality.WINDOW_MODAL);
+        popupWindow.show();
+    }
+    public void setTransferInfo(double amount, String accNo, String receiverName){
+        amountConfirm.setText(String.valueOf(amount));
+        accountNoConfirm.setText(accNo);
+        receiverConfirm.setText(receiverName);
+    }
 }
